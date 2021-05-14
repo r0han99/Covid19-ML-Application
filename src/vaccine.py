4530ab0840c5848bcd7b5dabe886c3a0767cc9ec
Rohan Sai <45916202+r0han99@users.noreply.github.com>
2021-05-14 22:07:00 +0530

    fixed vaccine calculation

--- src/vaccine.py
+++ src/vaccine.py
@@ -94,9 +94,9 @@ def vaccineStats(vaccine):
         
 
     
-        total = vaccine_df[vaccine_df['location'] == country]['total_vaccinations'][-1:].values[0]
-        full_vacc =  vaccine_df[vaccine_df['location'] == country]['people_fully_vaccinated'][-1:].values[0]
-        oneshot = vaccine_df[vaccine_df['location'] == country]['people_vaccinated'][-1:].values[0]
+        total = vaccine_df[vaccine_df['location'] == CountryName]['total_vaccinations'][-1:].values[0]
+        full_vacc = vaccine_df[vaccine_df['location'] == CountryName]['people_fully_vaccinated'][-1:].values[0]
+        oneshot = vaccine_df[vaccine_df['location'] == CountryName]['people_vaccinated'][-1:].values[0]
         last_date = vaccine_df[vaccine_df['location'] == country]['date'][-1:].values[0]
 
         fully_vacc = (full_vacc/total)*100
